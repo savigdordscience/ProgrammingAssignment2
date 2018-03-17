14508516e706ed05806ac2567cb60709401e8f71
savigdordscience <36956296+savigdordscience@users.noreply.github.com>
2018-03-17 21:08:36 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## This function is basically an object that manages 
+  ## a cache of an inversed matrix 
+  ## functions:
+  ##    getMatrix - returns the matrix unchanged
+  ##    setMatrix - sets a Matrix
+  ##    setCachedMatrix - keep the cached matrix
+  ##    getCachedMatrix - returns the cached matrix
+		cachedMatrix <- NULL
+        setMatrix <- function(y) {
+                x <<- y
+                cachedMatrix <<- NULL
+        }
+        getMatrix <- function() x
+        setCachedMatrix <- function(inverse) cachedMatrix <<- inverse
+        getCachedMatrix <- function() cachedMatrix
+        list(setMatrix = setMatrix, getMatrix = getMatrix,
+             setCachedMatrix = setCachedMatrix,
+             getCachedMatrix = getCachedMatrix)
 }
 
 
@@ -12,4 +29,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## The function stores the cached matrix in the 
+        ## in the x object & updates it with an inversed 
+        ## matrix if it has not been instantiated.
+		cachedMatrix <- x$getCachedMatrix()
+        if(!is.null(cachedMatrix)) {
+                message("getting cached matrix")
+                return(cachedMatrix)
+        }
+        matrix <- x$getMatrix()
+        cachedMatrix <- solve(matrix)
+        x$setCachedMatrix(cachedMatrix)
+        cachedMatrix
 }
